c5920aa6ddb2daef91c32232a0a884531c8ad296
Nour Melegy <nourmostafaasu@gmail.com>
2025-12-05 01:47:49 +0200

    insert method and add child handling

--- RadixTree.cpp
+++ RadixTree.cpp
@@ -1,59 +1,148 @@
 // RadixTree.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include <cstring>
 #include "RadixTree.h"
 
-	RadixTree::RadixTree(){
-		myRoot = nullptr;
-	}
-	RadixTree::~RadixTree() {
-		//deletion 
-	}
 
-	bool RadixTree::empty(){
-		return childCount == 0;
+RadixTree::RadixTree() {
+	myRoot = nullptr;
+}
+RadixTree::~RadixTree() {
+	//deletion 
+}
 
-	}
-	int RadixTree::SearchPrefix(const char& word, Node* currectNode) {
-		int i = 0;
-		while (currentNode->data[i] != '\0' && word[i] != '\0' && word[i] == currentNode->data[i])
-			i++;
-		return i;
-	}
-	void RadixTree::insert(const char* word) {
-		//case1:empty tree
-		if (empty()) {
-			myRoot=new Node(word);
-			myRoot->ended = true;
-			return;
-		}
-		//case 3:common  prefix found
-		child* cur = root->children;
-		bool foundPrefix = false;
+bool RadixTree::empty() {
+	return myRoot == nullptr;
 
-		while (cur != nullptr) {
-			int pref = SearchPrefix(cur->node->data, word);
-			if (pref> 0) {
-				foundPrefix = true;
-				break;
+}
+int RadixTree::SearchPrefix(const char* word, Node* currentNode) {
+	int i = 0;
+	while (currentNode->data[i] != '\0' && word[i] != '\0' && word[i] == currentNode->data[i])
+		i++;
+	return i;
+}
+void RadixTree::addchild(Node* parent, Node* childnode) {
+	child* newchild = new child(childnode->data[0], childnode);//saved first char, and childnode is the node of child itselt
+	newchild->next = parent->children;//siblings of new child is now the chilren of the parent
+	parent->children = newchild; //children of parent is now the new child, so when saying children the pointer now points to the new child
+}
+void RadixTree::insert(const char* word) {
+	//case1:empty tree
+	if (empty()) {
+		myRoot = new Node(word);
+		myRoot->ended = true;
+		return;
+	}
+	//case 3:common  prefix found
+	//child* cur = myRoot->children;
+	bool isPrefix = false;
+	//Node* current = myRoot;
+	Node* parentNode = myRoot;
+	child* ch = parentNode->children;
+	while (ch != nullptr) {
+		int nodeLen = 0;
+		while (ch->node->data[nodeLen] != '\0') nodeLen++;
+		int wordLen = 0;
+		while (word[wordLen] != '\0') wordLen++;
+		int prefix = SearchPrefix(word, ch->node);
+		if (prefix > 0) {// case exact match
+			isPrefix = true;
+			if (prefix == nodeLen && prefix == wordLen) {// this works becaus ethe serachprefix counts only equal prefixes so if they are same number as well then its the same word
+				ch->node->ended = true;
+				return;
 			}
-			cur = cur->next;
+			if (prefix == wordLen && prefix < nodeLen) {
+				Node* oldnode = ch->node;//let oldnode pointer point at the node with the whole word
+				Node* newnode = new Node(word); //node created for our word 
+				newnode->ended = true;
+				/*char temp[50]
+				strcpy(temp, oldnode->data+pref);
+				strcpy(oldnode->data,temp);
+				or */
+				Node* suffix = new Node(oldnode->data + prefix);
+				suffix->ended = oldnode->ended;
+				suffix->children = oldnode->children;
+				newnode->children = nullptr;
+				addchild(newnode, suffix);
+				ch->node = newnode;
+				delete oldnode;
+				return;
+
+			}
+				//case 2:common  prefix found
+		    /*if (wordLen > nodeLen) {
+				isPrefix = true;
+				for (int i = 0; i < nodeLen; i++) {
+					if (ch->node->data[i] != word[i]) {
+						isPrefix = false;
+						break;
+					}
+				}
+			if (isPrefix) {
+					int difference = wordLen - nodeLen;
+					Node* newNode = new Node();
+					for (int i = 0; i < difference; i++) {
+						newNode->data[i] = word[nodeLen + i];
+					}
+				    newNode->data[difference] = '\0';
+					newNode->ended = true;
+					newNode->children = nullptr;
+					child* newChild = new child(newNode->data[0], newNode);
+					newChild->next = ch->node->children;
+					ch->node->children = newChild;
+					addchild(ch->node, newNode);
+					return;
+				}
+			}*/
+			if (prefix == nodeLen && wordLen > nodeLen) {
+
+				Node* newNode = new Node(word + nodeLen);
+				newNode->ended = true;
+				newNode->children = nullptr;
+
+				addchild(ch->node, newNode);
+				return;
+			}
+			//else {//case 3:partial overlap condition
+				//int prefix = SearchPrefix(word, ch->node->data);
+			if (prefix > 0 && prefix < nodeLen) {
+				Node* newTop = new Node();
+				strncpy_s(newTop->data, ch->node->data, prefix);
+				newTop->data[prefix] = '\0';
+				newTop->ended = false;
+				Node* node1 = new Node();
+				Node* node2 = new Node();
+				strncpy_s(node1->data, ch->node->data + prefix, nodeLen - prefix);
+				node1->data[nodeLen - prefix] = '\0';
+				node1->ended = ch->node->ended;
+				node1->children = ch->node->children;
+				strncpy_s(node2->data, word + prefix, wordLen - prefix);
+				node2->data[wordLen - prefix] = '\0';
+				node2->ended = true;
+				node2->children = nullptr;
+				child* child1 = new child(node1->data[0], node1);
+				child* child2 = new child(node2->data[0], node2);
+				child1->next = child2;
+				newTop->children = child1;
+				//child* newChild= new child(newTop->data[0],newTop);
+				//newChild->next = ch->next;
+				//parentNode->children = newChild;
+				addchild(parentNode, newTop);
+				return;
+			}
 		}
-	//case 2:no common prefix found
-		if (!foundPrefix) {
-			Node* oldRoot = myRoot;
-			myRoot = new Node();
-			child* oldChild = new child(oldRoot->data[0], oldRoot);
-			Node* newNode = new Node(word);
-			newNode->ended = true;
-			child* newChild = new child(word[0], newNode);
-			oldChild->next = newChild;
-			myRoot->children = oldChild;
-
-
+		parentNode = ch->node;
+		ch = ch->next;
 
 	}
 
+	//case 2:no common prefix found
+		Node* newnode = new Node(word);// created a newnode pointer of type node, then made it point to a node that has the word
+		newnode->ended = true;
+		newnode->children = nullptr;
+		addchild(parentNode, newnode);
+	}
 
 
 
@@ -69,3 +158,4 @@
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
