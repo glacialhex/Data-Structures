df9cffd4429fd0f1fac3a0be88c72a559fb7905d
Nour Melegy <nourmostafaasu@gmail.com>
2025-12-07 14:39:23 +0200

    Add deleteWord and deleteRec methods to RadixTree

--- RadixTree.h
+++ RadixTree.h
@@ -11,5 +11,8 @@ public:
 	void insert(const char* word);
 int SearchPrefix(const char* word, Node* currectNode);
 void addchild(Node* parent, Node* childnode);
+bool deleteWord(const char* word) ;
+bool deleteRec(Node*& current, const char* word);
 };
+
 
