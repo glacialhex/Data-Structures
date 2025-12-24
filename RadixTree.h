0a0d9ba0ee85646c1123a304cac83813c9beebb4
Malak <183957415+Malakabdelhadii@users.noreply.github.com>
2025-12-24 13:40:44 +0200

    Enhance RadixTree with prefix search and suggestions
    Added new methods for prefix finding and auto-suggestion.

--- RadixTree.h
+++ RadixTree.h
@@ -8,19 +8,28 @@ public:
 	RadixTree();
 	~RadixTree();
 	bool empty();
-	void insert(const char* word);
+
+void insert(const char* word);
 int SearchPrefix(const char* word, Node* currectNode);
 void addchild(Node* parent, Node* childnode);
+
 bool deleteWord(const char* word) ;
 bool deleteRec(Node*& current, const char* word);
 
 // Search / Lookup Operation
-
+int matchPrefix(const char* nodePrefix, const char* keySegment);
 bool search(const char* key);
 Node* traverseEdge(Node* node, const char* keySegment, int& matchedLen);
-int matchPrefix(const char* nodePrefix, const char* keySegment);
 bool isKeyFullyMatched(Node* node, const char* key, int keyLen);
 void handleSearchFailure(const char* reason);
+
+
+Node* findNodeForPrefix(const char* prefix);
+void autoSuggest(const char* prefix, Node* current);
+void collectWords(Node* node, string currentWord) {
+
+
 };
+
 
 
